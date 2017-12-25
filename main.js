ed12abea81fcc07d86f6a7ebc53a5960af8da801
dariia-lobatenko <dariia.lobatenko@gmail.com>
2017-12-25 02:03:23 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,73 @@
+/**
+ * Created by Mauri on 25.12.17.
+ */
+
+window.onload = function(){
+
+    var step = 0;
+
+    document.getElementById("game").onclick = function(){
+        console.log(event);
+        if (event.target.className = 'block'){
+            if (step % 2 == 0){
+                event.target.innerHTML = "x";
+            }else{
+                event.target.innerHTML = "0";
+            }
+            step++;
+            checkWinner();
+        }
+    }
+    function checkWinner(){
+        var allblock = document.getElementsByClassName('block');
+        if (allblock[0].innerHTML == 'x' && allblock[1].innerHTML == 'x' && allblock[2].innerHTML == 'x' ){
+            alert("First player WIN!");
+        }
+        if (allblock[3].innerHTML == 'x' && allblock[4].innerHTML == 'x' && allblock[5].innerHTML == 'x' ){
+            alert("First player WIN!");
+        }
+        if (allblock[6].innerHTML == 'x' && allblock[7].innerHTML == 'x' && allblock[8].innerHTML == 'x' ){
+            alert("First player WIN!");
+        }
+        if (allblock[0].innerHTML == 'x' && allblock[3].innerHTML == 'x' && allblock[6].innerHTML == 'x' ){
+            alert("First player WIN!");
+        }
+        if (allblock[1].innerHTML == 'x' && allblock[4].innerHTML == 'x' && allblock[7].innerHTML == 'x' ){
+            alert("First player WIN!");
+        }
+        if (allblock[2].innerHTML == 'x' && allblock[5].innerHTML == 'x' && allblock[8].innerHTML == 'x' ){
+            alert("First player WIN!");
+        }
+        if (allblock[0].innerHTML == 'x' && allblock[4].innerHTML == 'x' && allblock[8].innerHTML == 'x' ){
+            alert("First player WIN!");
+        }
+        if (allblock[2].innerHTML == 'x' && allblock[4].innerHTML == 'x' && allblock[6].innerHTML == 'x' ){
+            alert("First player WIN!");
+        }
+        if (allblock[0].innerHTML == '0' && allblock[1].innerHTML == '0' && allblock[2].innerHTML == '0' ){
+            alert("Second player WIN!");
+        }
+        if (allblock[3].innerHTML == '0' && allblock[4].innerHTML == '0' && allblock[5].innerHTML == '0' ){
+            alert("Second player WIN!");
+        }
+        if (allblock[6].innerHTML == '0' && allblock[7].innerHTML == '0' && allblock[8].innerHTML == '0' ){
+            alert("Second player WIN!");
+        }
+        if (allblock[0].innerHTML == '0' && allblock[3].innerHTML == '0' && allblock[6].innerHTML == '0' ){
+            alert("Second player WIN!");
+        }
+        if (allblock[1].innerHTML == '0' && allblock[4].innerHTML == '0' && allblock[7].innerHTML == '0' ){
+            alert("Second player WIN!");
+        }
+        if (allblock[2].innerHTML == '0' && allblock[5].innerHTML == '0' && allblock[8].innerHTML == '0' ){
+            alert("Second player WIN!");
+        }
+        if (allblock[0].innerHTML == '0' && allblock[4].innerHTML == '0' && allblock[8].innerHTML == '0' ){
+            alert("Second player WIN!");
+        }
+        if (allblock[2].innerHTML == '0' && allblock[4].innerHTML == '0' && allblock[6].innerHTML == '0' ){
+            alert("Second player WIN!");
+        }
+    }
+
+}
