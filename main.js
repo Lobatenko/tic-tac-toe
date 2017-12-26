978c83f1209c85a03350b7439ef1286fb4d15f0b
dariia-lobatenko <dariia.lobatenko@gmail.com>
2017-12-26 15:01:32 +0200

    add reset cell after winner

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ window.onload = function(){
     var step = 0;
 
     document.getElementById("game").onclick = function(){
-        console.log(event);
+        //console.log(event);
         if (event.target.className = 'block'){
             if (step % 2 == 0){
                 event.target.innerHTML = "x";
@@ -18,57 +18,85 @@ window.onload = function(){
             checkWinner();
         }
     }
-    //jgkjgkjhkghg
+    // check Winner
     function checkWinner(){
         var allblock = document.getElementsByClassName('block');
         if (allblock[0].innerHTML == 'x' && allblock[1].innerHTML == 'x' && allblock[2].innerHTML == 'x' ){
             alert("First player WIN!");
+            reset();
         }
         if (allblock[3].innerHTML == 'x' && allblock[4].innerHTML == 'x' && allblock[5].innerHTML == 'x' ){
             alert("First player WIN!");
+            reset();
         }
         if (allblock[6].innerHTML == 'x' && allblock[7].innerHTML == 'x' && allblock[8].innerHTML == 'x' ){
             alert("First player WIN!");
+            reset();
         }
         if (allblock[0].innerHTML == 'x' && allblock[3].innerHTML == 'x' && allblock[6].innerHTML == 'x' ){
             alert("First player WIN!");
+            reset();
         }
         if (allblock[1].innerHTML == 'x' && allblock[4].innerHTML == 'x' && allblock[7].innerHTML == 'x' ){
             alert("First player WIN!");
+            reset();
         }
         if (allblock[2].innerHTML == 'x' && allblock[5].innerHTML == 'x' && allblock[8].innerHTML == 'x' ){
             alert("First player WIN!");
+            reset();
         }
         if (allblock[0].innerHTML == 'x' && allblock[4].innerHTML == 'x' && allblock[8].innerHTML == 'x' ){
             alert("First player WIN!");
+            reset();
         }
         if (allblock[2].innerHTML == 'x' && allblock[4].innerHTML == 'x' && allblock[6].innerHTML == 'x' ){
             alert("First player WIN!");
+            reset();
         }
         if (allblock[0].innerHTML == '0' && allblock[1].innerHTML == '0' && allblock[2].innerHTML == '0' ){
             alert("Second player WIN!");
+            reset();
         }
         if (allblock[3].innerHTML == '0' && allblock[4].innerHTML == '0' && allblock[5].innerHTML == '0' ){
             alert("Second player WIN!");
+            reset();
         }
         if (allblock[6].innerHTML == '0' && allblock[7].innerHTML == '0' && allblock[8].innerHTML == '0' ){
             alert("Second player WIN!");
+            reset();
         }
         if (allblock[0].innerHTML == '0' && allblock[3].innerHTML == '0' && allblock[6].innerHTML == '0' ){
             alert("Second player WIN!");
+            reset();
         }
         if (allblock[1].innerHTML == '0' && allblock[4].innerHTML == '0' && allblock[7].innerHTML == '0' ){
             alert("Second player WIN!");
+            reset();
         }
         if (allblock[2].innerHTML == '0' && allblock[5].innerHTML == '0' && allblock[8].innerHTML == '0' ){
             alert("Second player WIN!");
+            reset();
         }
         if (allblock[0].innerHTML == '0' && allblock[4].innerHTML == '0' && allblock[8].innerHTML == '0' ){
             alert("Second player WIN!");
+            reset();
         }
         if (allblock[2].innerHTML == '0' && allblock[4].innerHTML == '0' && allblock[6].innerHTML == '0' ){
             alert("Second player WIN!");
+            reset();
         }
     }
+
+    // reset cells
+     function reset(){
+        var allblock = document.getElementsByClassName('block');
+            for (var i = 0; i <9; i++){
+                allblock[i].innerHTML = " ";
+            }
+        }
+//    }
+//    function isDraw() {
+//        alert("Draw");
+//    }
 
 }
